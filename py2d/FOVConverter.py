0883f1ab8e8ec2e7a81a4c391220034a0de148f7
Stefan Seemayer <stefan@seemayer.de>
2012-02-29 13:00:47 +0100

    Initial commit

---
+++ py2d/FOVConverter.py
@@ -0,0 +1,229 @@
+from Math import *
+
+def convert_tilemap_generic(width, height, blocking_function, tile_width, tile_height):
+	"""Convert a tile-based map file to obstructors for FOV calculation
+	
+	Arguments:
+	
+	width			-- The width of the map in tiles
+	height			-- The height of the map in tiles
+
+	blocking_function	-- The function used to determine whether the tile at x,y blocks light. e.g.: lambda x,y: map.get_tile(x,y).block_light
+
+	tile_width		-- The width of a single tile
+	tile_height		-- The height of a single tile
+	"""
+
+
+	def find_clusters():
+		clusters = [[0 for i in range(height)] for j in range(width)]
+		cluster_count = 0
+		clusters_seen = set()
+
+		def rename_cluster(old, new):
+			#print "rename cluster %d --> %d" % (old, new)
+			clusters_seen.remove(old)
+			clusters_seen.add(new)
+			
+			for x in range(width):
+				for y in range(height):
+					if clusters[x][y] == old:
+						clusters[x][y] = new 
+
+		for x in range(width):
+			for y in range(height):
+
+				block = blocking_function(x,y)
+				block_left = blocking_function(x-1, y) if x > 0 else False
+				block_top = blocking_function(x,y-1) if y > 0 else False
+	
+
+				#print "x: %d\ty:%d" % (x,y)
+
+				# merge tiles to clusters of the same blocking status by
+				# assigning cluster numbers to them. Iterate over whole
+				# map array and compare blocking statuses of tiles to the
+				# left and to the top of the current tile. If they have
+				# the same blocking status, take their cluster information.
+				# If cluster informations differ, merge the clusters.
+				# If differing blocking information, create new cluster.
+					
+				if block == block_left and block == block_top and x > 0 and y > 0:
+					# clusters both to the left and the top.
+					# merge clusters by overwriting all occurrences of the left cluster with the top cluster
+					new_cluster = clusters[x][y-1]
+					old_cluster = clusters[x-1][y]
+
+					if new_cluster != old_cluster:
+						rename_cluster(old_cluster, new_cluster)
+					clusters[x][y] = new_cluster
+
+					#print "x: %d\ty:%d: join left %d -> top %d" % (x,y, old_cluster, new_cluster)
+				elif block == block_left and x > 0:
+					# add to left cluster
+					clusters[x][y] = clusters[x-1][y]
+					#print "x: %d\ty:%d: use left %d" % (x,y, clusters[x-1][y])
+
+				elif block == block_top and y > 0:
+					# add to top cluster
+					clusters[x][y] = clusters[x][y-1]
+					#print "x: %d\ty:%d: use top %d" % (x,y, clusters[x][y-1])
+
+				else:
+					# no adjacent clusters, create a new one
+					cluster_count += 1
+					#print "x: %d\ty:%d: new cluster %d" % (x,y, cluster_count)
+					clusters[x][y] = cluster_count
+					clusters_seen.add(cluster_count)
+
+
+
+
+
+		translation = {}
+		cs = list(clusters_seen)
+		for i in range(len(clusters_seen)):
+			translation[cs[i]] = i + 1
+
+		for x in range(width):
+			for y in range(height):
+				old = clusters[x][y]
+				if(old > 0): clusters[x][y] = translation[old]
+
+		return clusters, clusters_seen	
+
+	
+	def cluster_outline(cluster):
+
+		def get_startpos():
+			for x in range(width):
+				for y in range(height):
+					if clusters[x][y] == cluster:
+						return x,y
+			return None
+		
+		start_x, start_y = get_startpos()
+
+		# directions:
+		# 0: going right at the top of the cluster, 
+		# 1: going down at the right of the cluster,
+		# 2: going left at the bottom of the cluster, 
+		# 3: going up at the left of the cluster.
+		direction = 0
+
+		x, y = start_x, start_y
+		outline = [Vector(x * tile_width, y * tile_height)]
+
+		while True:
+
+			right  = clusters[x+1][y] if x + 1 < width else 0
+			top    = clusters[x][y-1] if y > 0 else 0
+			left   = clusters[x-1][y] if x > 0 else 0
+			bottom = clusters[x][y+1] if y + 1 < height else 0
+
+			if direction == 0:
+				# we are going right at the top of the cluster.
+
+				if right == cluster and top != cluster:
+					# everything is fine, follow along the path
+					x += 1;
+				elif top == cluster:
+					# we have a cluster member at the top. 
+					# go up now and add top-left corner to outline
+					direction = 3
+					outline += [Vector((x) * tile_width, (y) * tile_height)]
+					y -= 1;
+				else:
+					# we have no more cluster members to the right. 
+					# go down now and add top-right corner to outline
+					direction = 1
+					outline += [Vector((x+1) * tile_width, (y) * tile_height)]
+
+			elif direction == 1:
+				# we are going down at the right of the cluster.
+
+				if bottom == cluster and right != cluster:
+					# everything is fine, follow along the path
+					y += 1;
+				elif right == cluster:
+					# we have a cluster member to the right.
+					# go right now and add top-right corner to outline
+					direction = 0
+					outline += [Vector((x+1) * tile_width, (y) * tile_height)]
+					x += 1;
+				else:
+					# we have no more cluster members at the bottom.
+					# go left now and add bottom-right corner to outline
+					direction = 2
+					outline += [Vector((x+1) * tile_width, (y+1) * tile_height)]
+
+			elif direction == 2:
+				# we are going left at the bottom of the cluster.
+
+				if left == cluster and bottom != cluster:
+					# everything is fine, follow along the path
+					x -= 1
+				elif bottom == cluster:
+					# we have a cluster member at the bottom.
+					# go down now and add bottom-right corner to outline
+					direction = 1
+					outline += [Vector((x+1) * tile_width, (y+1) * tile_height)];
+					y += 1;
+				else:
+					# we have no more cluster members at the left.
+					# go up now and add bottom-left corner to outline
+					direction = 3
+					outline += [Vector((x) * tile_width, (y+1) * tile_height)]
+			else:
+				# we are going up at the left of the cluster.
+
+				if top == cluster and left != cluster:
+					# everything is fine, follow along the path
+					y -= 1
+				elif left == cluster:
+					# we have a cluster member to the left.
+					# go left now and add bottom-left corner to outline
+					direction = 2
+					outline += [Vector((x) * tile_width, (y+1) * tile_height)]
+					x -= 1;
+				else:
+					# we have no more cluster members at the top.
+					# go right now and add top-left corner to outline
+					direction = 0
+					outline += [Vector((x) * tile_width, (y) * tile_height)]
+			
+			# find if we've made a full loop and have come back to the start
+			if direction == 0 and x == start_x and y == start_y:
+				#outline += [outline[1]]
+				break
+
+		return outline
+
+	
+	clusters, clusters_seen = find_clusters()
+	cluster_outlines = []
+
+	for cluster in range(1, len(clusters_seen) + 1):
+		cluster_outlines += [cluster_outline(cluster)]
+
+	return cluster_outlines
+
+
+if __name__ == '__main__':
+
+	map_data = [[1,1,1,1,0,0,0,0],
+		    [1,0,0,1,0,0,1,0],
+		    [1,0,1,1,0,1,1,1],
+		    [1,0,1,1,0,0,1,0],
+		    [1,1,1,0,0,0,1,0],
+		    [0,0,0,0,0,1,1,1],
+		    [0,1,1,1,0,0,1,0]]
+
+	for line in map_data:
+		print line
+
+	outlines = convert_tilemap_generic(len(map_data), len(map_data[0]), lambda x,y: map_data[x][y], 1,1)
+
+	for outline in outlines: 
+		print outline
+
---
+++ py2d/Math.py
@@ -0,0 +1,221 @@
+import math
+
+class Vector:
+
+	def __init__(self, x, y):
+		self.x = x
+		self.y = y
+
+	def get_length(self):
+		return math.sqrt(self.get_length_squared())
+
+	def get_length_squared(self):
+		return self.x * self.x + self.y * self.y;
+	
+	def normalize(self):
+		return self / self.get_length()
+
+	def clamp(self):
+		if self.get_length() > 1:
+			return self.normalize()
+		else:
+			return self
+
+	def clone(self):
+		return Vector(self.x, self.y)
+
+	def as_tuple(self):
+		return (self.x, self.y)
+
+	def __add__(self, b):
+		return Vector(self.x + b.x, self.y + b.y)
+	
+	def __sub__(self, b):
+		return Vector(self.x - b.x, self.y - b.y)
+
+	def __mul__(self, val):
+
+		if isinstance(val, Vector):
+			return self.x * val.x + self.y * val.y
+		else:
+			return Vector(self.x * val, self.y * val)
+	
+	def __div__(self, val): 
+		return Vector(self.x / val, self.y / val)
+
+	def __repr__(self):
+		return "Vector(%f, %f)" % (self.x, self.y)
+	
+	def __eq__(self, other):
+		d = self - other
+		return abs(d.x) <  0.0001 and abs(d.y) < 0.0001
+
+
+	def __ne__(self, other):
+		return not self.__eq__(other)
+
+	def __hash__(self):
+		return hash("%.4f %.4f" % (self.x, self.y))
+
+class Polygon:
+
+	def __init__(self):
+		self.points = []
+
+	@staticmethod
+	def regular(center, radius, points):
+
+		angular_increment = 2 * math.pi / points
+
+		p = Polygon()
+		for i in range(points):
+			p.add_point( Vector(center.x + radius * math.cos(i * angular_increment), center.y + radius * math.sin(i * angular_increment)) )
+
+		return p
+
+	def add_point(self, point):
+		self.points += [point]
+
+	def add_points(self, points):
+		self.points += points
+
+	def get_centerpoint(self):
+		xes = [p.x for p in self.points]
+		yes = [p.y for p in self.points]
+
+		return Vector(sum(xes) / len(xes), sum(yes) / len(yes) )
+
+	def sort_around(self, center):
+
+		def angle_from_origin(p):
+			phi = math.acos(p.x / p.get_length())
+			if p.y < 0: phi = 2 * math.pi - phi
+			return phi 
+	
+	
+		self.points.sort(key=lambda p: angle_from_origin(p - center))
+
+	def __repr__(self):
+		pts = ["(%.2f, %.2f)" % (p.x, p.y) for p in self.points]
+		return "Polygon [%s]" % ", ".join(pts)
+
+
+	
+
+def __intersect_line_line_u(p1, p2, q1, q2):
+
+
+	d = (q2.y - q1.y) * (p2.x - p1.x) - (q2.x - q1.x) * (p2.y - p1.y)
+	n1 = (q2.x - q1.x) * (p1.y - q1.y) - (q2.y - q1.y) * (p1.x - q1.x)
+	n2 = (p2.x - p1.x) * (p1.y - q1.y) - (p2.y - p1.y) * (p1.x - q1.x)
+
+	if d == 0: return None
+
+	u_a = float(n1) / d
+	u_b = float(n2) / d
+
+	return (u_a, u_b)
+
+def intersect_poly_lineseg(poly_points, p1, p2):
+	return intersect_linesegs_lineseg(zip(poly_points[0:], poly_points[1:]) + [(poly_points[-1], poly_points[0])], p1, p2)
+
+def intersect_poly_ray(poly_points, p1, p2):
+	return intersect_linesegs_ray(zip(poly_points[0:], poly_points[1:]) + [(poly_points[-1], poly_points[0])], p1, p2)
+
+def intersect_line_line(p1, p2, q1, q2):
+	ll = __intersect_line_line_u(p1, p2, q1, q2)
+
+	if ll == None: return None
+	return Vector(p1.x + ll[0] * (p2.x - p1.x) , p1.y + ll[0] * (p2.y - p1.y) ) 
+
+def intersect_lineseg_line(p1, p2, q1, q2):
+
+	ll = __intersect_line_line_u(p1, p2, q1, q2)
+
+	if ll == None: return None
+	if ll[0] < 0 or ll[0] > 1: return None
+
+	return Vector(p1.x + ll[0] * (p2.x - p1.x) , p1.y + ll[0] * (p2.y - p1.y) ) 
+
+def intersect_lineseg_ray(p1, p2, q1, q2):
+
+	ll = __intersect_line_line_u(p1, p2, q1, q2)
+
+	if ll == None: return None
+	if ll[0] < 0 or ll[0] > 1: return None
+	if ll[1] < 0: return None
+
+	return Vector(p1.x + ll[0] * (p2.x - p1.x) , p1.y + ll[0] * (p2.y - p1.y) ) 
+
+def intersect_linesegs_ray(segs, p1, p2):
+	intersect_points = []
+
+	for line_segment in segs:
+		intersect = intersect_lineseg_ray(line_segment[0], line_segment[1], p1, p2)
+		if intersect:
+			if line_segment[0] != p2 and line_segment[1] != p2:
+				intersect_points += [intersect]
+
+	return intersect_points
+
+def intersect_linesegs_lineseg(segs, p1, p2):
+	intersect_points = []
+
+	for line_segment in segs:
+		intersect = intersect_lineseg_lineseg(line_segment[0], line_segment[1], p1, p2)
+		if intersect:
+			if line_segment[0] != p2 and line_segment[1] != p2:
+				intersect_points += [intersect]
+	
+	return intersect_points
+
+def intersect_linesegs_linesegs(segs1, segs2):
+	intersect_points = []
+	for ls1 in segs1:
+		intersect_points += intersect_linesegs_lineseg(segs2, ls1[0], ls1[1])
+
+	return intersect_points
+
+def intersect_lineseg_lineseg(p1, p2, q1, q2):
+
+	if max(q1.x, q2.x) < min(p1.x, p2.x): return None
+	if min(q1.x, q2.x) > max(p1.x, p2.x): return None
+	if max(q1.y, q2.y) < min(p1.y, p2.y): return None
+	if min(q1.y, q2.y) > max(p1.y, p2.y): return None
+
+	ll = __intersect_line_line_u(p1, p2, q1, q2)
+
+	if ll == None: return None
+	if ll[0] < 0 or ll[0] > 1: return None
+	if ll[1] < 0 or ll[1] > 1: return None
+
+	return Vector(p1.x + ll[0] * (p2.x - p1.x) , p1.y + ll[0] * (p2.y - p1.y) ) 
+
+def check_intersect_lineseg_lineseg(p1, p2, q1, q2):
+
+	if max(q1.x, q2.x) < min(p1.x, p2.x): return False
+	if min(q1.x, q2.x) > max(p1.x, p2.x): return False
+	if max(q1.y, q2.y) < min(p1.y, p2.y): return False
+	if min(q1.y, q2.y) > max(p1.y, p2.y): return False
+
+	ll = __intersect_line_line_u(p1, p2, q1, q2)
+
+	if ll == None: return False
+	if ll[0] < 0 or ll[0] > 1: return False
+	if ll[1] < 0 or ll[1] > 1: return False
+
+	return True
+
+def distance_point_lineseg_squared(p, a, b):
+
+	ap_squared = (p - a).get_length_squared()
+	bp_squared = (p - b).get_length_squared()
+	ap_prime = a * b
+	
+	perpendicular_squared = ap_squared - ap_prime * ap_prime
+
+	return min(ap_squared, bp_squared, perpendicular_squared)
+
+
+VECTOR_NULL = Vector(0,0)
+
