c37825249fc6ec5f1ca20161a2396f9bba13bea2
Stefan Seemayer <stefan@seemayer.de>
2012-03-08 19:52:20 +0100

    Add FOV example

---
+++ py2d/examples/FOV.py
@@ -0,0 +1,50 @@
+import pygame
+from pygame.locals import *
+
+from py2d.FOV import *
+from py2d.Math import *
+import py2d.examples.Main
+
+class FOV(py2d.examples.Main.Example):
+
+	def __init__(self, runner):
+		self.runner = runner
+		self.title = "Field of View"
+		self.help = """
+		Field of View Example
+		-----------------------------------------
+	
+		Move your mouse and watch how the obstacles affect the field of view
+
+		Have fun!
+		"""
+
+		box = [Vector(-15, -2), Vector(15,-2), Vector(15,2), Vector(-15,2), Vector(-15,-2)]
+
+		center = Vector( py2d.examples.Main.SCREEN_WIDTH / 2, py2d.examples.Main.SCREEN_HEIGHT / 2)
+
+		# create circle of boxes around center
+		self.obstructors = [ [ Transform.move( center.x, center.y ) * Transform.rotate(phi) * Transform.move(100,0) * p for p in box ] for phi in [i * 2.0 * math.pi / 16 for i in range(16)]]
+		
+		self.eye = center
+		self.vision = Vision(self.obstructors)
+
+	def update(self, time_elapsed):
+		self.fov = self.vision.get_vision(self.eye, VISION_RADIUS, Polygon.regular(self.eye, VISION_RADIUS, VISION_POINTS))
+
+	def render(self):
+		if len(self.fov) > 3:
+			pygame.draw.polygon(self.runner.screen, 0xFFFF66, self.fov.as_tuple_list())
+			pygame.draw.polygon(self.runner.screen, 0xFFFFFF, self.fov.as_tuple_list(), 1)
+
+		for obs in self.obstructors:
+			pygame.draw.lines(self.runner.screen, 0xFF0000, True, [ p.as_tuple() for p in obs ])
+
+
+	def mouse_move(self, pos, rel, buttons): 
+		# add 0.01 to avoid colinearity
+		self.eye = Vector(pos[0]+0.01, pos[1]+0.01)
+
+
+VISION_RADIUS = 300
+VISION_POINTS = 16
