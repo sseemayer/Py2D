d6d9a6b7654ded435d7964f7109b0ece65ea7f55
Stefan Seemayer <stefan@seemayer.de>
2012-03-12 23:33:41 +0100

    Improved jekyll_watch.sh

--- jekyll_watch.sh
+++ jekyll_watch.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+
 while true; do
 	jekyll
-	inotifywait -e modify *.md *.html documentation/*.md css/*.css _layouts/*.htm
+
+
+	find . \( -path "./_site" -o -path "./.git" -o -name "*.swp" \) -prune -o -type f -print | xargs inotifywait -e modify
 done
